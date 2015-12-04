f69b9729f720e1286bac5bde497d87f6ddf9e165
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2015-12-04 19:41:55 +0100

    deploy on meteor.com

--- App.jsx
+++ App.jsx
@@ -1,30 +1,55 @@
 // App component - represents the whole app
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
-  },
 
-  renderTasks() {
-    return this.getTasks().map((task) => {
-      return <Task key={task._id} task={task} />;
-    });
-  },
+    // This mixin makes the getMeteorData method work
+    mixins: [ReactMeteorData],
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
+    getMeteorData() {
+        return {
+            tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+        }
+    },
 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+    handleSubmit(event){
+        event.preventDefault();
+        var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        if(text === ""){
+            return;
+        }
+
+        Tasks.insert({
+            text: text,
+            createdAt : new Date()
+        });
+
+        ReactDOM.findDOMNode(this.refs.textInput).value = "";
+
+    },
+    renderTasks() {
+        return this.data.tasks.map((task) => {
+            return <Task key={task._id} task={task} />;
+        });
+    },
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+
+                    <form className="new-task" onSubmit={this.handleSubmit} >
+                        <input
+                            type="text"
+                            ref="textInput"
+                            placeholder="Type to add new tasks" />
+                    </form>
+                </header>
+
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
+        );
+    }
 });
